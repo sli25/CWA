7f7a9279cfacdbdf64779e0802f2c387345a105a
Marko97IT <concas.marco997@gmail.com>
2018-10-19 22:39:43 +0200

    First commit

---
+++ CheckActivation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CWA
+{
+    using SLID = Guid;
+    public class CheckActivation
+    {
+
+        public enum SL_GENUINE_STATE
+        {
+            SL_GEN_STATE_IS_GENUINE = 0,
+            SL_GEN_STATE_INVALID_LICENSE = 1,
+            SL_GEN_STATE_TAMPERED = 2,
+            SL_GEN_STATE_LAST = 3
+        }
+
+        [DllImportAttribute("Slwga.dll", EntryPoint = "SLIsGenuineLocal", CharSet = CharSet.None, ExactSpelling = false, SetLastError = false, PreserveSig = true, CallingConvention = CallingConvention.Winapi, BestFitMapping = false, ThrowOnUnmappableChar = false)]
+        [PreserveSig()]
+        internal static extern uint SLIsGenuineLocal(ref SLID slid, [In, Out] ref SL_GENUINE_STATE genuineState, IntPtr val3);
+
+
+        public static bool IsGenuineWindows()
+        {
+            bool _IsGenuineWindows = false;
+            Guid ApplicationID = new Guid("55c92734-d682-4d71-983e-d6ec3f16059f");
+            SLID windowsSlid = (Guid)ApplicationID;
+            try
+            {
+                SL_GENUINE_STATE genuineState = SL_GENUINE_STATE.SL_GEN_STATE_LAST;
+                uint ResultInt = SLIsGenuineLocal(ref windowsSlid, ref genuineState, IntPtr.Zero);
+                if (ResultInt == 0)
+                {
+                    _IsGenuineWindows = (genuineState == SL_GENUINE_STATE.SL_GEN_STATE_IS_GENUINE);
+                }
+                else
+                {
+                    Console.WriteLine("Error getting information {0}", ResultInt.ToString());
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return _IsGenuineWindows;
+        }
+
+    }
+}
